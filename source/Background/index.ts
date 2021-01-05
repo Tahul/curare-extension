beca6f14557b19a738b6d0f6a30723c342f481c4
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-05 22:00:33 +0100

    :sparkles: (hot-reload) add crx-hotreload to background.ts

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -1,6 +1,8 @@
+import 'crx-hotreload/hot-reload.js'
 import 'emoji-log'
 import { browser } from 'webextension-polyfill-ts'
 
+
 browser.runtime.onInstalled.addListener((): void => {
-  console.emoji('🦄', 'extension installed')
+  console.emoji('🦄', 'Curare extension installed.')
 })
