26f864a27c312cfa1118e06add950acd40b77ad4
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-05 23:16:52 +0100

    :building_construction: (parts) moving app parts to destination parts folder

---
+++ source/parts/Background/index.ts
@@ -0,0 +1,7 @@
+import 'crx-hotreload/hot-reload.js'
+import 'emoji-log'
+import { browser } from 'webextension-polyfill-ts'
+
+browser.runtime.onInstalled.addListener((): void => {
+  console.emoji('🦄', 'Curare extension installed.')
+})
---
+++ source/parts/Popup/Popup.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { Switch } from 'react-router-dom'
+import './styles.scss'
+
+const Popup: React.FC = () => {
+  return <Switch></Switch>
+}
+
+export default Popup
