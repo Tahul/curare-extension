d325d91868964ecb98bd74c0eaa46d8c8397517d
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-05 23:16:32 +0100

    :building_construction: (parts) moving app parts to parts folder

--- source/Background/index.ts
+++
@@ -1,8 +0,0 @@
-import 'crx-hotreload/hot-reload.js'
-import 'emoji-log'
-import { browser } from 'webextension-polyfill-ts'
-
-
-browser.runtime.onInstalled.addListener((): void => {
-  console.emoji('🦄', 'Curare extension installed.')
-})
---
+++ source/parts/Popup/index.tsx
@@ -0,0 +1,17 @@
+import { createBrowserHistory } from 'history'
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import { Router } from 'react-router-dom'
+import Popup from './Popup'
+
+// Routing
+export const history = createBrowserHistory()
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router history={history}>
+      <Popup />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('popup-root'),
+)
