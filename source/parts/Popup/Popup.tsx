1557165ca294fbd0e4bfe739c1e290e6ebe16de9
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-06 04:13:20 +0100

    :sparkles: (popup) implement basic routes and popup base styling

--- source/parts/Popup/Popup.tsx
+++ source/parts/Popup/Popup.tsx
@@ -1,9 +1,43 @@
 import * as React from 'react'
 import { Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import PublicRoute from '../../components/utils/PrivateRoute'
+import PrivateRoute from '../../components/utils/PublicRoute'
+import { AuthProvider, useAuthState } from '../../contexts/auth'
+import useQueryLogout from '../../hooks/useQueryLogout'
+import Login from '../../views/Login'
 import './styles.scss'
 
+const StyledApp = styled.div`
+  height: 300px;
+  width: 300px;
+`
+
+// Router switch
+const Routes = () => {
+  const { isLoggedIn } = useAuthState()
+
+  useQueryLogout()
+
+  return (
+    <Switch>
+      {!isLoggedIn ? (
+        <PublicRoute restricted={true} path="/" component={Login} />
+      ) : (
+        <PrivateRoute path="/" component={Login} />
+      )}
+    </Switch>
+  )
+}
+
 const Popup: React.FC = () => {
-  return <Switch></Switch>
+  return (
+    <StyledApp>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </StyledApp>
+  )
 }
 
 export default Popup
