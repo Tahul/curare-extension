0f5ff2acea5e2bbf0dab638e0c1abbbff200baac
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-06 02:59:16 +0100

    :sparkles: (login) import login view

---
+++ source/views/Login.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import LoginForm from '../components/auth/LoginForm'
+import Page from '../components/utils/Page'
+import { useAuthState } from '../contexts/auth'
+
+const StyledLogin = styled.div``
+
+const Login = () => {
+  const { isLoggedIn } = useAuthState()
+  const history = useHistory()
+
+  if (isLoggedIn) {
+    history.push('/')
+  }
+
+  return (
+    <Page>
+      <StyledLogin>
+        <LoginForm />
+      </StyledLogin>
+    </Page>
+  )
+}
+
+export default Login
