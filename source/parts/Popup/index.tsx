ca6925e9899a2768a2e0f7e7f36448d2bc620675
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-07 02:04:44 +0100

    :sparkles: (popup) put authprovider above router, and use MemoryHistory instead of Browser one

--- source/parts/Popup/index.tsx
+++ source/parts/Popup/index.tsx
@@ -1,7 +1,8 @@
-import { createBrowserHistory } from 'history'
+import { createMemoryHistory } from 'history'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router-dom'
+import { AuthProvider } from '../../contexts/auth'
 import Popup from './Popup'
 import redraw from './redraw'
 
@@ -9,13 +10,15 @@ import redraw from './redraw'
 redraw()
 
 // Routing
-export const history = createBrowserHistory()
+export const history = createMemoryHistory()
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
-      <Popup />
-    </Router>
+    <AuthProvider>
+      <Router history={history}>
+        <Popup />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('popup-root'),
 )
