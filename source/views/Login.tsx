c8e9512ae73f2b617332e71f7061769165bef8e5
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-08 05:12:20 +0100

    :lipstick: (auth) add default padding to login view

--- source/views/Login.tsx
+++ source/views/Login.tsx
@@ -1,3 +1,4 @@
+import { theme } from '@heetch/flamingo-react'
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
@@ -5,7 +6,9 @@ import LoginForm from '../components/auth/LoginForm'
 import Page from '../components/utils/Page'
 import { useAuthState } from '../contexts/auth'
 
-const StyledLogin = styled.div``
+const StyledLogin = styled.div`
+  padding: ${theme.space.l};
+`
 
 const Login = () => {
   const { isLoggedIn } = useAuthState()
