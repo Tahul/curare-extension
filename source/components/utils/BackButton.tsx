1efce34aa6a76368b2cd98e9c2a4eeea73067469
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-08 05:09:11 +0100

    :sparkles: (utils) import and type back button component

---
+++ source/components/utils/BackButton.tsx
@@ -0,0 +1,48 @@
+import { Icon, Text, theme } from '@heetch/flamingo-react'
+import React from 'react'
+import styled from 'styled-components'
+import useActionsSounds from '../../hooks/useActionsSounds'
+
+const StyledBackButton = styled.div`
+  cursor: pointer;
+
+  .f-Text {
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${theme.color.element.tertiary};
+    }
+  }
+
+  .f-Icon {
+    margin-right: ${theme.space.s};
+  }
+`
+
+export interface BackButtonProps {
+  onBack: () => void
+  children: React.ReactNode
+}
+
+const BackButton: React.FC<BackButtonProps> = ({ onBack, children }) => {
+  const { playBack } = useActionsSounds()
+
+  const handleBack = () => {
+    playBack()
+
+    onBack()
+  }
+
+  return (
+    <StyledBackButton className="backButton">
+      <Text variant="contentBold" onClick={handleBack}>
+        <Icon icon="IconArrowLeft" />
+
+        {children}
+      </Text>
+    </StyledBackButton>
+  )
+}
+
+export default BackButton
