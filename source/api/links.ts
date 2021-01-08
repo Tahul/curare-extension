ab822a2c04f8a994ca40c1b4068f2b795767e78b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-08 03:28:36 +0100

    :fire: (links) unnecessary code for extension

--- source/api/links.ts
+++ source/api/links.ts
@@ -2,30 +2,9 @@ import { Link } from '../hooks/useLinks'
 import API from './index'
 
 /**
- * Get all the links for a user and/or a collection.
+ * Create a link.
  *
- * @param {string, string} userId
- */
-export const index = async ({
-  userId,
-  collectionId,
-}: {
-  userId?: string
-  collectionId?: string
-}): Promise<Link[]> => {
-  const request = await API.get(
-    `/links${collectionId ? `?collectionId=${collectionId}` : ``}${
-      userId ? `?userId=${userId}` : ``
-    }`,
-  )
-
-  return request.data as Link[]
-}
-
-/**
- * Create a collection.
- *
- * @param {title} collection
+ * @param {url} link
  */
 export const store = async ({
   url,
@@ -42,49 +21,12 @@ export const store = async ({
 }
 
 /**
- * Update a collection.
- *
- * @param {string, string} collection
- */
-export const update = async ({
-  id,
-  url,
-  ogp,
-}: Partial<Link>): Promise<Link> => {
-  const request = await API.patch(`/links/${id}`, { url, ogp })
-
-  return request.data as Link
-}
-
-/**
- * Destroy a collection.
- *
- * @param {string} id
- */
-export const destroy = async ({ id }: Partial<Link>): Promise<Link> => {
-  const request = await API.delete(`/links/${id}`)
-
-  return request.data as Link
-}
-
-/**
  * Preview a link OpenGraph data.
  *
  * @param {string} url
  */
 export const preview = async ({ url }: Partial<Link>): Promise<any> => {
   const request = await API.get(`/links/preview?url=${url}`)
-
-  return request.data
-}
-
-/**
- * Increment the click count of a link.
- *
- * @param {string} id
- */
-export const click = async ({ id }: Partial<Link>): Promise<Link> => {
-  const request = await API.post(`/links/${id}/click`)
 
   return request.data
 }
