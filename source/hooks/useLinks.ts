d7949e5db6982779a049968e850a66ca30c7ec37
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-08 03:29:17 +0100

    :fire: (links) prune unnecesary logic

--- source/hooks/useLinks.ts
+++ source/hooks/useLinks.ts
@@ -1,8 +1,7 @@
-import { useCallback, useEffect, useState } from 'react'
-import { click, destroy, index, preview, store, update } from '../api/links'
+import { useState } from 'react'
+import { preview, store } from '../api/links'
 import useActionsSounds from './useActionsSounds'
 import { Collection } from './useCollections'
-import useIsMounted from './useIsMounted'
 import { Profile } from './useProfile'
 
 export type Link = {
@@ -17,43 +16,9 @@ export type Link = {
   profile?: Partial<Profile>
 }
 
-type UseLinksHookProps = {
-  userId?: string
-  collectionId?: string
-}
-
-const useLinks = ({ userId, collectionId }: UseLinksHookProps = {}) => {
+const useLinks = () => {
   const { playSuccess, playError } = useActionsSounds()
-  const isMounted = useIsMounted()
   const [loading, setLoading] = useState(false)
-  const [links, setLinks] = useState<Link[]>([])
-
-  /**
-   * Get all the links for the current userId.
-   *
-   * @param {boolean} isMounted
-   */
-  const getLinks = useCallback(async () => {
-    if (isMounted) setLoading(true)
-
-    let links: Link[] | undefined
-
-    try {
-      if (collectionId) {
-        links = await index({ collectionId })
-      } else if (userId) {
-        links = await index({ userId })
-      }
-
-      if (links) setLinks([...links])
-    } catch (e) {
-      console.log(e)
-    }
-
-    if (isMounted) setLoading(false)
-
-    return links
-  }, [collectionId, userId, isMounted])
 
   /**
    * Create a link.
@@ -67,10 +32,6 @@ const useLinks = ({ userId, collectionId }: UseLinksHookProps = {}) => {
 
     try {
       if (url) link = await store({ url, ogp, collection_id })
-      else throw new Error()
-
-      if (link) setLinks([...links, link])
-      else throw new Error()
 
       playSuccess()
     } catch (e) {
@@ -80,66 +41,6 @@ const useLinks = ({ userId, collectionId }: UseLinksHookProps = {}) => {
     setLoading(false)
 
     return link
-  }
-
-  /**
-   * Update a link.
-   *
-   * @param {string, string} string
-   */
-  const updateLink = async ({ id, url }: Partial<Link>) => {
-    if (!url || !id) return
-
-    setLoading(true)
-
-    let updatedLink: Link | undefined
-
-    try {
-      updatedLink = await update({ id, url })
-
-      setLinks(
-        links.map((link) => {
-          if (updatedLink && link.id === updatedLink.id) {
-            return updatedLink
-          }
-
-          return link
-        }),
-      )
-
-      playSuccess()
-    } catch (e) {
-      playError()
-    }
-
-    setLoading(false)
-
-    return updatedLink
-  }
-
-  /**
-   * Delete a link.
-   *
-   * @param {id} link
-   */
-  const deleteLink = async ({ id }: Partial<Link>) => {
-    try {
-      await destroy({ id })
-
-      setLinks(
-        links.filter((link) => {
-          return link.id !== id
-        }),
-      )
-
-      playSuccess()
-
-      return { id }
-    } catch (e) {
-      playError()
-    }
-
-    return false
   }
 
   /**
@@ -159,49 +60,8 @@ const useLinks = ({ userId, collectionId }: UseLinksHookProps = {}) => {
     return false
   }
 
-  /**
-   * Increment the click count of a link.
-   *
-   * @param {string} id
-   */
-  const clickLink = async ({ id }: Partial<Link>) => {
-    if (!id) return
-
-    try {
-      const updatedLink = await click({ id })
-
-      setLinks(
-        links.map((link) => {
-          if (link.id === updatedLink.id) {
-            return updatedLink
-          }
-
-          return link
-        }),
-      )
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  useEffect(() => {
-    if (userId) {
-      const fetchLinks = async () => {
-        await getLinks()
-      }
-
-      fetchLinks()
-    }
-  }, [getLinks, userId])
-
   return {
-    links,
-    setLinks,
-    getLinks,
     createLink,
-    updateLink,
-    deleteLink,
-    clickLink,
     getLinkPreview,
     loading,
   }
