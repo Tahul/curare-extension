35f2ef751833e7986b2b1a1d50ca3f34743a4cbd
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-17 22:45:18 +0100

    :sparkles: (extension) add small validator for tab current url

--- source/hooks/useCurrentUrl.ts
+++ source/hooks/useCurrentUrl.ts
@@ -1,6 +1,15 @@
 import { useEffect, useState } from 'react'
 import useIsMounted from './useIsMounted'
 
+/**
+ * Simple validator for the URL.
+ *
+ * @param url
+ */
+const isValidUrl = (url: string) => {
+  return !url.includes('chrome-extension://')
+}
+
 export default () => {
   const isMounted = useIsMounted()
   let [currentUrl, setCurrentUrl] = useState<string>()
@@ -9,12 +18,10 @@ export default () => {
     chrome.tabs.query(
       { active: true, lastFocusedWindow: true },
       (tabs: chrome.tabs.Tab[]) => {
-        if (tabs[0].url && isMounted) setCurrentUrl(tabs[0].url)
+        if (tabs[0].url && isMounted && isValidUrl(tabs[0].url))
+          setCurrentUrl(tabs[0].url)
       },
     )
-
-    // Debug purposes
-    if (!currentUrl && isMounted) setCurrentUrl('https://github.com/Tahul')
   }, [isMounted])
 
   return { currentUrl }
