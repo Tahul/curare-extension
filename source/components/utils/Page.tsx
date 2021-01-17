82c02c01a24bd2075bbb392b48f55ffeaae1d9bd
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-17 21:32:51 +0100

    :sparkles: (page) defaults page height to 100%

--- source/components/utils/Page.tsx
+++ source/components/utils/Page.tsx
@@ -10,6 +10,7 @@ const Page = ({
 }) => {
   return (
     <motion.div
+      style={{ height: '100%' }}
       initial={animated ? { opacity: 0.25, y: 50 } : {}}
       animate={animated ? { opacity: 1, y: 0 } : {}}
       exit={animated ? { opacity: 0, y: 50 } : {}}
